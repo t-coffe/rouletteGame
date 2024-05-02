f71fdc2645c2af31fdd5a2195a4b27abc4afd749
t-coffe <tcoffe@edenschool.fr>
2024-05-02 19:29:34 +0200

    the roulette can turn, it still needs improvement

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 import './style.css'
+
+const roulette = document.querySelector('.roulette');
+const btnTurn = document.querySelector('.roulette button');
+let old = 0;
+
+btnTurn.addEventListener('click', ()=> {
+    const change = (old + Math.floor(Math.random() * 360))%360;
+    roulette.style.transform = `rotate(${change+350}deg)`
+})
